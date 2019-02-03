9dae715d50723fb41efd2e714097bfa99fa6eddd
rishabhscross <parammittal16@gmail.com>
2019-02-04 02:24:45 +0530

    Add final changes

--- src/app/Components/ta-home/ta-home.component.ts
+++ src/app/Components/ta-home/ta-home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
+import * as AOS from 'aos';
+
 import { DataService } from 'src/app/Services/data.service';
 
 @Component({
@@ -12,11 +14,16 @@ export class TaHomeComponent implements OnInit {
   model: any = {};
   constructor(private d: DataService) { }
   ngOnInit() {
+    AOS.init();
     this.d.getData(36)
     .subscribe(res => {
       this.data = res;
       console.log(this.data);
     }, err => console.log(err));
+  }
+  scrollToElement($element): void {
+    console.log($element);
+    $element.scrollIntoView({behavior: 'smooth', block: 'start', inline: 'nearest'});
   }
   onSubmit() {
     console.log(this.model);
